e88cc4e811fcc18ecdaad5fa3dfe9843f8ab3d45
drfricke <46977167+drfricke@users.noreply.github.com>
2020-04-29 13:48:57 -0400

    Comments Added

--- main.py
+++ main.py
@@ -9,31 +9,31 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 import ubinascii, ujson, urequests, utime
 
-# Write your program here
-ev3 = EV3Brick()
-#ev3.speaker.beep()
 
 #pycam set up
 from pybricks.iodevices import AnalogSensor, UARTDevice
 uart = UARTDevice(Port.S1, 9600, timeout=2000)
 
+#On Board motors
 rightMotor = Motor(Port.A)
 leftMotor = Motor(Port.D)
-
 clawMotor = Motor(Port.B)
-#tilt = GyroSensor(Port.S1) #Stops is derails??
 
+#Sensor and Start up values
 dist = UltrasonicSensor(Port.S4)
 kp = 0.21
 travel = 'off'
 Motor_input = -150 # pos towards kitchen, neg towards living room
 
+
+#API Setup
 def SL_setup():
-    Key = 'bvd8X9LweQY9o2eP1NYL-p8mLL9wMAk6YYOnYSiIo0'
+    Key = #####
     urlBase = "https://api.systemlinkcloud.com/nitag/v2/tags/"
     headers = {"Accept":"application/json","x-ni-api-key":Key}
     return urlBase, headers   
 
+#Put Function for API
 def Put_SL(Tag, Type, Value):
      urlBase, headers = SL_setup()
      urlValue = urlBase + Tag + "/values/current"
@@ -44,7 +44,7 @@ def Put_SL(Tag, Type, Value):
           print(e)         
           reply = 'failed'
      return reply
-
+#Get Function for recieving cloud data
 def Get_SL(Tag):
     urlBase, headers = SL_setup()
     urlValue = urlBase + Tag + "/values/current"
@@ -62,7 +62,7 @@ def Get_SL(Tag):
         print(e)
         result = 'failed'
     return result 
-
+#Ball release, reset motors
 def release():
     rightMotor.stop(stop_type=Stop.COAST)
     leftMotor.stop(stop_type=Stop.COAST)
@@ -70,13 +70,13 @@ def release():
     clawMotor.run_angle(200, 60)
     travel = 'off'
     return travel
-
+#location reset
 def reset():
     rightMotor.reset_angle(0)
     leftMotor.reset_angle(0)
     rightMotor.run_target(200,30*360,wait=False)
     leftMotor.run_target(200,30*360)
-
+#Waiting for remote trigger
 def waiting():
     while True:
         if Get_SL('Start08') == 'true':
@@ -88,7 +88,7 @@ def waiting():
                 data = uart.read()
                 print(data.decode('utf-8'))
     return travel
-
+#Payload drop, find the wall
 def cleanUp():
     while True:
         print(dist.distance())
@@ -101,14 +101,16 @@ def cleanUp():
             reset()
             break
         
-
+#Main Code
 travel = waiting()
 
 while travel == 'on':
+    #Checking for obstacles
     if dist.distance() > 150:
         Motor_input = -150
     if dist.distance() <= 150:
         Motor_input = -10
+    #Serial Commands from cam
     if uart.waiting() >= 1:
         data = uart.read()
         if data.decode('utf-8') == 'y':
@@ -116,15 +118,9 @@ while travel == 'on':
             wait(1000)
             release()
             break
-
+    #Keeps Motors rolling
     rightMotor.run(Motor_input)
     leftMotor.run(Motor_input)
 
 
 cleanUp()
-
-#reset to true???
-#Put_SL('Start08', 'BOOLEAN', 'true')
-
-# for i in range(10):
-#     print(dist.distance())
