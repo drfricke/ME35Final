605d3b0cdfbfbdd77595cf677d23a22cda2bae7d
drfricke <46977167+drfricke@users.noreply.github.com>
2020-04-29 14:39:14 -0400

    Comments added

--- MachineLearningComponent.py
+++ MachineLearningComponent.py
@@ -23,16 +23,9 @@ data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 #start up variables
 frame, image, = None, None
 
-#get password
-##def get_key():
-##    fin = open('Practice.txt')
-##    for element in fin:
-##        return element
-
-Key = 'bvd8X9LweQY9o2eP1NYL-p8mLL9wMAk6YYOnYSiIo0'
+Key = #########
 
 #System link setup, gets, and puts
-
 def SL_setup():
      Key = 'bvd8X9LweQY9o2eP1NYL-p8mLL9wMAk6YYOnYSiIo0'
      urlBase = "https://api.systemlinkcloud.com/nitag/v2/tags/"
@@ -62,8 +55,8 @@ def Put_SL(Tag, Type, Value):
           reply = 'failed'
      return reply
 
-#funtion that handles predition model
 
+#funtion that handles predition model
 def prediction(frame):
     #resize the image to a 224x224 with the same strategy as in TM2:
     #resizing the image to be at least 224x224 and then cropping from the center
@@ -71,41 +64,36 @@ def prediction(frame):
     #Need to change cv2 image accordingly
     frame = Image.fromarray(frame)
     image = ImageOps.fit(frame, size, Image.ANTIALIAS)
-
     #turn the image into a numpy array
     image_array = np.asarray(image)
-
-    # display the resized image
-    #image.show()
-
     # Normalize the image
     normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
     # Load the image into the array
     data[0] = normalized_image_array
 
     # run the inference and give predition
     prediction = model.predict(data)
     print(prediction)
-    #print(np.argmax(prediction))
+    #return index of most likely prediction
     shape_index = np.argmax(prediction)
     return np.argmax(prediction)
 
 #Turns camera on
 image = cv2.VideoCapture(0)
 
-
+#System Check - waits for remote user
 while True:
     if Get_SL('Start08') == 'false':
         ret, frame = image.read()
         frame = frame[380:600,520:780]
-        cv2.imshow('frame',frame)  
+        cv2.imshow('frame',frame)
         print('waiting')
         time.sleep(0.5)
     if Get_SL('Start08') == 'true':
         print('go!')
         break
 
+#Main Loop
 while(True):
     # Capture frame-by-frame
     ret, frame = image.read()
@@ -116,8 +104,10 @@ while(True):
     
     #Allows user to excute commands and quit
     key = cv2.waitKey(1) & 0xFF
+    #local user check
     if key  == ord('q'):
         prediction(frame)
+    #local user terminate code
     if key == ord('p'):
         image.release()
         cv2.destroyAllWindows()
@@ -125,15 +115,16 @@ while(True):
         break
     #Allows EV3 to interact and get predictions via sys link
     if Get_SL('Start08') == 'false':
-       time.sleep(1)
+       #EV3 will reset system link when the ball is released
+       #This engages image capture on laptop
+       time.sleep(2) #Time for ball to fall
        ball = prediction(frame)
        if ball == 0:
            print('Package Recieved')
            image.release()
            cv2.destroyAllWindows()
-           #Put_SL('Start09', 'BOOLEAN', 'true')
+           Put_SL('Start09', 'BOOLEAN', 'true')
            break
-           
        if ball == 1:
            print('Target Missed')
            
